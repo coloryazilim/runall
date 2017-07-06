64120335eb33f8138f6cf698e6b0b6df58beabec
Yasar icli <hello@yasaricli.com>
2017-07-06 20:04:36 +0300

    filters hidden

--- ui/client/helpers.js
+++ ui/client/helpers.js
@@ -6,6 +6,8 @@ Template.sites.helpers({
   settings() {
     return {
       rowsPerPage: 10,
+      showNavigation: 'never',
+      showFilter: false,
       fields: [
         { key: '_id', label: 'ID' },
         { key: 'url', label: 'URL' }
@@ -29,10 +31,14 @@ Template.caches.helpers({
   settings() {
     return {
       rowsPerPage: 10,
+      showNavigation: 'never',
+      showFilter: false,
       fields: [
         { key: 'url', label: 'Cache Url' },
         { key: 'status', label: 'Status' },
-        { key: 'statusCode', label: 'Status Code' }
+        { key: 'statusCode', label: 'Status Code' },
+        { key: 'downloadStarted', label: 'Start' },
+        { key: 'downloadFinished', label: 'End' }
       ]
     }
   }
